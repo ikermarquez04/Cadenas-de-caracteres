2a9c91415a33743fba9e9f96ca7f370ad9907bc9
ikerm <ikerm@LAPTOP-155AQCB8>
2023-01-12 22:05:18 -0600

    Agregar archivos de proyecto.

---
+++ Cadenas de caracteres/Cadenas de caracteres.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char palabra[] = "liam";
+    char palabra[] = { 'A','l','e,','j','a','n','d','r','o' };
+    char palabra[20];
+
+    cout << "digite su nombre: ";
+    cin.getline(nombre, 20, '\n');
+
+    cout << nombre << endl;
+
+    getch();
+    return 0;
+}
